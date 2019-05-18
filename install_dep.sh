429ce695e99d29737f5144b2c3b2893d558aaf16
MD-AZMAL <azmalmd12@gmail.com>
2019-05-18 11:01:46 +0530

    Version error fix

--- install_dep.sh
+++ install_dep.sh
@@ -2,7 +2,7 @@
 
 wget https://dl.google.com/go/go1.12.2.linux-amd64.tar.gz
 sudo tar -xvf go1.12.2.linux-amd64.tar.gz
-https://github.com/MD-AZMAL/install-dep
+sudo chown -R root:root ./go
 sudo mv go /usr/local
 echo "export GOROOT=/usr/local/go" >> ~/.profile
 echo "Enter go project name to setup GOPATH : "
@@ -10,7 +10,8 @@ read name
 echo "export GOPATH=$HOME/Projects/$name" >> ~/.profile
 echo "GOPATH set to : $HOME/Projects/$name"
 echo -e "\n"
-
+echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.profile
+source ~/.profile
 
 sudo apt-get install curl python-software-properties
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
